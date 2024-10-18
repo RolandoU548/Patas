45e85259d3febd2a5213952c2a3015972a7fb6e3
Rolando <rolandou548@gmail.com>
2024-10-18 04:22:10 -0400

    dev turbo

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="es" suppressHydrationWarning={true}>
+    <html lang="es" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased theme-rose`}
       >
