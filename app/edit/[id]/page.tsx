5893d5eef7c5eec7557ed0a9db9dfcfdb70fd3e8
Rolando <rolandou548@gmail.com>
2025-01-26 22:46:08 -0400

    guardar dibujo editado

--- app/edit/[id]/page.tsx
+++ app/edit/[id]/page.tsx
@@ -1,9 +1,16 @@
-import prisma from "@/lib/prisma";
 import CanvasComponent from "@/components/edit/Canvas";
+import { getDrawingById } from "@/app/actions";
+import ErrorComponent from "@/app/error";
 export default async function App({ params }: { params: { id: string } }) {
   const { id } = params;
-  const drawing = await prisma.drawing.findFirst({ where: { id } });
-
-  if (drawing?.imageUrl)
-    return <CanvasComponent url={drawing.imageUrl}></CanvasComponent>;
+  const drawing = await getDrawingById(id);
+  if (drawing) {
+    if ("error" in drawing) {
+      console.error(drawing.error);
+      return (
+        <ErrorComponent description="Ha ocurrido un error al editar el dibujo" />
+      );
+    }
+    return <CanvasComponent drawing={drawing}></CanvasComponent>;
+  }
 }
