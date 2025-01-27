e00ffe7d901d89997f4bc2d8e2faf43e0e236381
Rolando <rolandou548@gmail.com>
2025-01-26 22:42:52 -0400

    guardar dibujo editado

--- components/edit/Canvas.tsx
+++ components/edit/Canvas.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useRef, useState, useEffect, ReactNode } from "react";
-import { Canvas, Rect, Triangle, Ellipse, Line, FabricImage } from "fabric";
+import { Canvas, Rect, Triangle, Ellipse, FabricImage } from "fabric";
 import { Button } from "@/components/ui/button";
 import { FaRegSquare, FaRegCircle } from "react-icons/fa";
 import { FiTriangle } from "react-icons/fi";
@@ -8,10 +8,14 @@ import { DEFAULT_CANVAS_COLOR, DEFAULT_SHAPES_COLOR } from "@/lib/constants";
 import { DEFAULT_CANVAS_WIDTH, DEFAULT_CANVAS_HEIGHT } from "@/lib/constants";
 import { ShapeOptions } from "@/components/edit/ShapeOptions";
 import { CanvasSettings } from "@/components/edit/CanvasSettings";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 import {
   handleObjectMoving,
   clearGuideLines,
 } from "@/components/edit/SnappingHelpers";
+import { uploadDrawingToFirebase } from "@/lib/utils";
+import { Drawing } from "@prisma/client";
 
 const canvasWidth = DEFAULT_CANVAS_WIDTH;
 const canvasHeight = DEFAULT_CANVAS_HEIGHT;
@@ -25,15 +29,18 @@ const triangleHeight = 86;
 const ellipseWidth = 100;
 const ellipseHeight = 100;
 
-const App = ({ url }: { url: string }) => {
+const App = ({ drawing }: { drawing: Drawing }) => {
   const canvasRef = useRef(null);
   const [canvas, setCanvas] = useState<Canvas | null>(null);
-  const [guidelines, setGuideLines] = useState<Line[]>([]);
 
   const addImage = async (canvas: Canvas, url: string) => {
-    const image = await FabricImage.fromURL(url);
+    const image = await FabricImage.fromObject({
+      src: url,
+      crossOrigin: "anonymous",
+    });
     image.top = canvas.height / 2 - image.height / 2;
     image.left = canvas.width / 2 - image.width / 2;
+    image.fill = "#000000";
     canvas?.add(image);
     canvas?.renderAll();
   };
@@ -51,14 +58,13 @@ const App = ({ url }: { url: string }) => {
       setCanvas(initCanvas);
 
       initCanvas.on("object:moving", (event) => {
-        handleObjectMoving(initCanvas, event.target, guidelines, setGuideLines);
+        handleObjectMoving(initCanvas, event.target);
       });
-
       initCanvas.on("object:modified", () => {
         clearGuideLines(initCanvas);
       });
 
-      addImage(initCanvas, url);
+      addImage(initCanvas, drawing.imageUrl);
 
       return () => {
         initCanvas.dispose();
@@ -147,16 +153,46 @@ const App = ({ url }: { url: string }) => {
         ref={canvasRef}
       />
       <ShapeOptions canvas={canvas} />
+
+      <div className="mt-2 flex flex-col gap-2 items-center">
+        <Label htmlFor="image">Agrega una imagen</Label>
+        <Input
+          className=""
+          onChange={(e) => {
+            if (e.target.files) {
+              addImage(
+                canvas as Canvas,
+                URL.createObjectURL(e.target.files[0])
+              );
+            }
+          }}
+          type="file"
+          id="image"
+          name="image"
+          accept="image/png, image/jpeg, image/gif"
+        />
+      </div>
       <Button
         onClick={() => {
-          if (canvas)
-            addImage(
-              canvas,
-              "https://firebasestorage.googleapis.com/v0/b/patas-49960.appspot.com/o/drawings%2Fd3254f2b-8773-44d8-9604-c9ac726db637?alt=media&token=1b93aa6b-da06-4950-8a85-5eb31ded62ef"
-            );
+          const dataUrl = canvas?.toDataURL({ format: "png", multiplier: 1 });
+          if (dataUrl === null || dataUrl == undefined) return;
+          const arr = dataUrl.split(",");
+          const mimeMatch = arr[0].match(/:(.*?);/);
+          const mime = mimeMatch ? mimeMatch[1] : "";
+          const bstr = atob(arr[1]);
+          let n = bstr.length;
+          const u8arr = new Uint8Array(n);
+
+          while (n--) {
+            u8arr[n] = bstr.charCodeAt(n);
+          }
+          uploadDrawingToFirebase(
+            drawing.id,
+            new File([u8arr], drawing.title, { type: mime })
+          );
         }}
       >
-        AGREGAR
+        Guardar
       </Button>
     </main>
   );
